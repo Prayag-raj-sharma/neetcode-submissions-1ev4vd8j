4c784bf96196e36e96113c15ef217f255e724c1a
Prayag Raj Sharma <86906091+Prayag-raj-sharma@users.noreply.github.com>
2026-04-03 19:35:54 +0530

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+    int isBal(TreeNode* root) {
+        if(!root) return 0;
+
+        int left = isBal(root->left);
+
+        if(left == -1) return -1;
+
+        int right = isBal(root->right);
+
+        if(right == -1) return -1;
+
+        if(abs(left - right) > 1) return -1;
+        return 1 + max(left, right);
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        return isBal(root) != -1;
+        
+    }
+};
