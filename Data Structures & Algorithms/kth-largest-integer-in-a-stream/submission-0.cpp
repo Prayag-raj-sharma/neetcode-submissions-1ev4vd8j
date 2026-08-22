8d0409215e9204098391a33b0bd1a07055d6f564
Prayag Raj Sharma <86906091+Prayag-raj-sharma@users.noreply.github.com>
2026-08-22 12:29:42 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,25 @@
+class KthLargest {
+private: 
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int _k;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        _k = k;
+        for(int num: nums) {
+            minHeap.push(num);
+
+            if(minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }  
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+
+        if(minHeap.size() > _k) {
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
