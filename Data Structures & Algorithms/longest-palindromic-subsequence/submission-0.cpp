f1e2e89001904cd1ef647b71fa0750e3334cd09e
Prayag Raj Sharma <86906091+Prayag-raj-sharma@users.noreply.github.com>
2026-08-25 12:13:50 +0530

    Add: longest-palindromic-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-subsequence/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int lcs(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i] == t[j]) {
+            return dp[i][j] = 1 + lcs(i - 1, j - 1, s, t, dp);
+        }
+
+        return dp[i][j] = max(lcs(i - 1, j, s, t, dp), lcs(i, j - 1, s, t, dp));
+
+    }
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+
+        string t = s;
+        reverse(s.begin(), s.end());
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return lcs(n - 1, n - 1, s, t, dp);
+    }
+};
