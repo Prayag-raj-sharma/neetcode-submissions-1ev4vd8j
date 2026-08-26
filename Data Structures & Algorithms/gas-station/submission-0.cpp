36a522db18b24191ede9e583feb3a29faa07408c
Prayag Raj Sharma <86906091+Prayag-raj-sharma@users.noreply.github.com>
2026-08-26 18:39:12 +0530

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int start = 0;
+        int tank = 0;
+        int total = 0;
+
+        for(int i = 0; i < n; i++) {
+            int gain = gas[i] - cost[i];
+
+            total += gain;
+            tank += gain;
+
+            if(tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return total >= 0 ? start : -1; 
+    }
+};
